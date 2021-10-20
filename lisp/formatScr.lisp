7e2f4a0a98dc3b293e6219d68815192de7c066b1
Adrian S. Faircloth <faircloth_as@cobra.cs.mercer.edu>
2021-10-19 23:58:20 -0400

    Completed code for finding longest and shortest lines

--- lisp/formatScr.lisp
+++ lisp/formatScr.lisp
@@ -9,7 +9,7 @@
 		)
 	)
 )
-
+;;TODO: Make functions for checking for longest and shortest lines
 (defvar filename)
 (defvar longlist)
 ;;creating string list to hold partly-formatted lines
@@ -82,30 +82,99 @@
 ;;Reverse wordlist so list pops words in order
 (setq wordlist (reverse wordlist))
 ;;Building formatted lines of text w/ line number and 60 characters max
+;;Uses word counters to find lines w/ most and least number of words
 (defvar templine)
 (setq templine "")
 (defvar tempword)
 (setq tempword (pop wordlist))
-
+(defvar numwords)
+(setq numwords 0)
+(defvar mostwords)
+(setq mostwords 0)
+(defvar leastwords)
+(setq leastwords 0)
+(defvar longest-line)
+(setq longest-line " ")
+(defvar shortest-line)
+(setq shortest-line " ")
 (loop while (/= (length wordlist) 0)
 	do(progn
+	  ;;If the length of the line w/ the next word would break 60,
+	  ;;	push line to longlist, reset word counter and set
+	  ;;	templine to empty so it can hold the next line
 	  (if (> (+ (length templine) (length tempword)) 60)
 		(progn
 		(push templine longlist)
+		;;If current line has more words than last found
+		;;	longest line, then store new longest line
+		;;If there is a tie, stores line w/ least chars
+		(if (> numwords mostwords)
+			(progn
+			(setq longest-line templine)
+			(setq mostwords numwords)))
+		(if (= numwords mostwords)
+			(progn
+			(if (> (length longest-line) (length templine))
+				(setq longest-line templine))))
+		;;If current line has fewer words than last found
+		;;	shortest line, then store new shortest line
+		;;If there is a tie, stores line w/ least chars
+		(if (or (= leastwords 0) (< numwords leastwords))
+			(progn
+			(setq shortest-line templine)
+			(setq leastwords numwords)))
+		(if (= numwords leastwords)
+			(progn
+			(if (> (length shortest-line) (length templine))
+				(setq shortest-line templine))))
+				
+		(setq numwords 0)
 		(setq templine "")))
+	  ;;If length will not break 60, add next word to line
+	  ;;	and increment word counter
 	  (setq templine (concatenate 'string templine tempword))
+	  (incf numwords)
+	  ;;Adds space only if line is less than 60 characters
 	  (if (< (length templine) 60)
 	  	(setq templine (concatenate 'string templine " ")))
 	  (setq tempword (pop wordlist))))
- 
+;;After popping final word from wordlist, adds word to line if it
+;;	fits and pushes line
+;;If word does not fit, pushes current line then pushes final word
 (if (>= (+ (length templine) (length tempword)) 60)
 	(progn
 	(push templine longlist)
-	(push tempword longlist))
+	(if (> numwords mostwords)
+		(progn
+		(setq longest-line templine)
+		(setq mostwords numwords)))
+	(if (= numwords mostwords)
+		(progn
+		(if (> (length longest-line) (length templine))
+			(setq longest-line templine))))
+	(if (or (= leastwords 0) (< numwords leastwords))
+		(progn
+		(setq shortest-line templine)
+		(setq leastwords numwords)))
+	(if (= numwords leastwords)
+		(progn
+		(if (> (length shortest-line) (length templine))
+			(setq shortest-line templine))))
+	(setq templine tempword)
+		(if (< 1 leastwords)
+			(setq shortest-line templine))
+		(if (= 1 leastwords)
+			(progn
+			(if (> (length shortest-line) (length templine))
+				(setq shortest-line templine))))
+	(push templine longlist))
+
 	(progn
 	(setq templine (concatenate 'string templine tempword))
-	(push templine longlist)))
+	(push templine longlist)
+	(if (> numwords mostwords)
+		(setq longest-line templine))
+	(if (< numwords leastwords)
+		(setq shortest-line templine))))
+;;Reversing longlist so lines are in order
 (setq longlist (reverse longlist))
-(loop for line in longlist
-	do(progn
-	(write-line line)))
